5440691fce9923d889172a266009fa51f83348ad
Mateusz Choma <kontakt@coderoad.pl>
2021-07-28 18:42:28 +0200

    Using `<>` tag as fragment

--- src/Items.js
+++ src/Items.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-export const Item = () => {
+export const Items = () => {
   return (
-    <React.Fragment>
-      <div key={'1'}>Item 1</div>
-      <div key={'2'}>Item 2</div>
-      <div key={'3'}>Item 3</div>
-    </React.Fragment>
+    <>
+      <div>Item 1</div>
+      <div>Item 2</div>
+      <div>Item 3</div>
+    </>
   )
 }
 
-export default Item
+export default Items
